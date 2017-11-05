523540f22e3efafd3616eedf7e0da3de2176fcc4
Balakrishnan Venkatesan <bv98@afsconnect1.njit.edu>
2017-11-05 14:30:01 -0500

    display records

--- index.php
+++ index.php
@@ -5,12 +5,32 @@
  $password = "dvJjpozdZ";
  $connect = NULL;
 
-try {
+ try {
     $connect = new PDO("mysql:host=$hostname;dbname=bv98",$username,$password);
     echo "Connected successfully <br>";
-}
-catch(PDOException $e) {
+    $query = 'SELECT * FROM accounts WHERE `id` < 6';
+    $rows = runQuery($query);
+    echo count($rows) . ' records have id < 6 <br>';
+    $header = 'SHOW COLUMNS FROM accounts';
+    $head = runQuery($header);
+    echo table($head,$rows);
+ }
+ catch(PDOException $e) {
     http_error("500 Internal Server Error\n\n" . "Error in connecting to the DB:\n\n" . $e->getMessage() . "<br>");
-}
+ }
+
+ function runQuery($query){
+    global $connect;
+    try {
+        $sql = $connect->prepare($query);
+        $sql->execute();
+        $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+        $sql->closeCursor();
+        return $result;
+    }
+    catch(PDOException $e) {
+        http_error("500 Internal Server Error\n\n" . "Error in connecting to the DB:\n\n" . $e->getMessage() . "<br>");
+    }
+ }
 
 ?>
